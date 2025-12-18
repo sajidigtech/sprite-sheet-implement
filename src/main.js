1d2c21219f85ebc4c74e40843c79b195f726ab15
Md Sajid Ali <msajidali@trueigtech.com>
2025-12-18 17:11:37 +0530

    bomb sprite added in the game and functioning is there

--- src/main.js
+++ src/main.js
@@ -8,13 +8,25 @@ await app.init({
 document.getElementById("canvas").appendChild(app.canvas);
 const sheet = await Assets.load("/src/assets/player.json")
 const player = new AnimatedSprite(sheet.animations.run);
-player.animationSpeed = 0.25;
+player.animationSpeed = 0.40;
 player.loop = true;
 player.play();
 player.anchor.set(0.5);
 player.x = app.screen.width / 2;
 player.y = app.screen.height / 2;
 app.stage.addChild(player);
+
+
+const bombSheet = await Assets.load("/src/assets/bomb.json");
+const bomb = new AnimatedSprite(bombSheet.animations.blast);
+bomb.animationSpeed = 0.5;
+bomb.loop = true;
+bomb.anchor.set(0.5);
+bomb.x = player.x + 40;
+bomb.y = player.y +30;
+// bomb.visible = true;
+bomb.play();
+app.stage.addChild(bomb);
 
 
 
@@ -25,8 +37,18 @@ const stopBtn = document.getElementById("stopBtn");
 
 playBtn.addEventListener("click", () => {
   player.play();
+  bomb.visible = true;
+  bomb.gotoAndPlay(0);
 });
+// bomb.onComplete = () => {
+//   bomb.visible = false;
+// };
 
 stopBtn.addEventListener("click", () => {
   player.stop();
 });
+
+
+function playerjump(){
+  
+}
